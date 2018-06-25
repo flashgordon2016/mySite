84e99c2e491ed7cb5b070ffd56d98adc6781d351
Matthew Morse <morsmatt@oregonstate.edu>
2018-06-25 15:52:33 -0800

    Created 'about me page' and started work on sidebar.

--- views/about-me.php
+++ views/about-me.php
@@ -11,33 +11,38 @@
     <title>M. Gordon Morse</title>
   </head>
   <body class="light-mode">
+    <!-- *************************************************************************
+                                  top horizontal navbar
+    ************************************************************************** -->
     <?php include("./shared/topnav.php"); ?>
+    <!-- Sidebar and page content wrappers -->
+    <div class="container-fluid">
+      <div class="row">
+        <!-- *********************************************************************
+                                    responsive sidebar
+        ********************************************************************** -->
+        <?php include("./shared/sidenav.php"); ?>
+        <!-- *********************************************************************
+                                      page content
+        ********************************************************************** -->
+        <div class="col-sm-12 page-content">
+          <div class="row">
+            <div class="col-xl">
+              <span class="h3 page-title">about me <?php include("../images/Octicons/three-bars.svg"); ?></span>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script type="text/javascript">
-      //Credit https://harlemsquirrel.github.io/css/javascript/2017/01/31/dark-light-mode-switcher.html
-      function toggleBackground(){
-        //Toggle black/white
-        $('body').toggleClass('light-mode dark-mode');
-        //Navbar
-        $('#top-menu').toggleClass('navbar-dark bg-dark navbar-light bg-light');
-        //Buttons
-        //Credit: https://stackoverflow.com/questions/7002039/easiest-way-to-toggle-2-classes-in-jquery
-        $('#light-btn').toggle();
-        $('#dark-btn').toggle();
-        $('.btn').toggleClass('btn-outline-light btn-outline-dark');
-        //SVGS (done)
-        //Set session variable accordingly
-      }
-
-      $(document).ready(function(){
-        //Listener for click of dark/light button
-        $('#light-btn').on('click', toggleBackground);
-        $('#dark-btn').on('click', toggleBackground);
-      });
+    <script>
+      $("#aboutMeBtn").addClass("active"); //add active class to Business Insights button
     </script>
+    <script type="text/javascript" src="../scripts/toggleBackground.js"></script>
   </body>
 </html>
---
+++ views/shared/sidenav.php
@@ -0,0 +1,33 @@
+<div id="sidebar">
+
+  <span class="h3 ml-3 sidebar-title" style="vertical-align:middle;">about me <?php include("../images/Octicons/x.svg"); ?></span>
+
+  <hr>
+  <div class="btn-group-vertical">
+    <!-- Home button -->
+    <a class="btn btn-light" href="admin-home.php" id="homeBtn">
+      <?php include("../images/Octicons/home.svg"); ?>
+      <div class="sidebar-text">experience</div>
+    </a>
+    <!-- education button -->
+    <a class="btn btn-light" href="manage-users.php" id="manageUsersBtn">
+      <?php include("../images/Octicons/mortar-board.svg"); ?>
+      <div class="sidebar-text">education</div>
+    </a>
+    <!-- Manage admin button -->
+    <a class="btn btn-light" href="manage-admin.php" id="manageAdminBtn">
+      <?php include("../images/Octicons/shield.svg"); ?>
+      <div class="sidebar-text">skills</div>
+    </a>
+    <!-- Business insights button -->
+    <a class="btn btn-light" href="admin-biz.php" id="BIbtn">
+      <?php include("../images/Octicons/graph.svg"); ?>
+      <div class="sidebar-text">hobbies</div>
+    </a>
+    <!-- Logout button -->
+    <a class="btn btn-light" href="http://18.188.194.159/award-hub/login-system/killSession.php">
+      <?php include("../images/Octicons/fold.svg"); ?>
+      <div class="sidebar-text">collapse</div>
+    </a>
+  </div>
+</div>
